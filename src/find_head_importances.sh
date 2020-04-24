a299327bc638906c8920f1b411516985e115de62
sai-prasanna <sai.r.prasanna@gmail.com>
2020-04-24 20:39:03 +0200

    Add Head importance scores without pruning and its analysis.

---
+++ src/find_head_importances.sh
@@ -0,0 +1,25 @@
+export GLUE_DIR=../data/glue
+mkdir -p ../finetuned_models/
+
+for TASK_NAME in "CoLA" "SST-2" "MRPC" "STS-B" "QQP" "MNLI" "QNLI" "RTE" "WNLI"
+do
+    echo "Masking" $TASK_NAME
+    for i in 1337,0 42,1 86,2 71,3 166,4
+    do
+        IFS=","; set -- $i;
+        SEED=$1
+        GPU=$2
+        OUTPUT_DIR=../head_importance_scores/$TASK_NAME/seed_$SEED/
+        mkdir -p $OUTPUT_DIR
+        export CUDA_VISIBLE_DEVICES=$GPU
+        python run_bertology.py \
+                --model_type bert \
+                --model_name_or_path ../finetuned_models/$TASK_NAME/seed_$SEED/ \
+                --task_name $TASK_NAME \
+                --data_dir $GLUE_DIR/$TASK_NAME \
+                --max_seq_length 128 \
+                --output_dir $OUTPUT_DIR \
+                --seed $SEED \
+                --batch_size 64 \
+    done
+done
